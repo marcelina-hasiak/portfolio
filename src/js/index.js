82844ed59d30fedd024698781cdffb3d3c8ff585
Marcelina Hasiak <marcelina.hasiak@gmail.com>
2020-06-23 22:54:15 +0200

    Add hamburger menu functionality. Refactoring media queries

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,22 @@
 import '../scss/main.scss';
+console.log('dupaaaa')
+/* HAMBURGER MENU */
 
-/* place your code below */
+const hamburgerButton = document.querySelector('.hamburger-menu--js')
+const navigation = document.querySelector('.navigation--js')
+const navigationBar = document.querySelector('.navigation__bar--js')
+const hamburgerBackground = document.querySelector('.hamburger-menu-bg--js')
 
-console.log('HELLO 🚀')
+hamburgerButton.addEventListener('click', () => {
+    navigation.classList.add('navigation--open')
+    navigationBar.classList.add('navigation__bar--open')
+    hamburgerBackground.classList.add('hamburger-menu-bg--open')
+    hamburgerButton.classList.add('closed')
+})
+
+hamburgerBackground.addEventListener('click', (e) => {
+    navigation.classList.remove('navigation--open')
+    navigationBar.classList.remove('navigation__bar--open')
+    hamburgerBackground.classList.remove('hamburger-menu-bg--open')
+    hamburgerButton.classList.remove('closed')
+})
