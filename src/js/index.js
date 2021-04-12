94620c5bfabc45eab6dd9dd71bcf4a3b3c5f437d
Marcelina <marcelina.hasiak@gmail.com>
2021-04-13 00:00:24 +0200

    Add hover effect at hero section, replace codepen icon with linkedin icon, change links to sites with icons

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
 import '../scss/main.scss';
-import svg from "../assets/img/logo.svg";
+
 /* CAROUSEL HANDLER */
 new Glide('.glide', {
     type: 'carousel',
@@ -36,74 +36,34 @@ const navigationBar = document.querySelector('.navigation__bar--js')
 const navigationLinks = document.querySelectorAll('.navigation__link--js')
 const hamburgerBackground = document.querySelector('.hamburger-menu-bg--js')
 
-hamburgerButton.addEventListener('click', () => {
-    navigation.classList.add('navigation--open')
-    navigationBar.classList.add('navigation__bar--open')
-    hamburgerBackground.classList.add('hamburger-menu-bg--open')
-    hamburgerButton.classList.add('closed')
-    body.classList.add('prevent-scrolling')
-})
+hamburgerButton.addEventListener('click', hamburgerMenuHandler)
+hamburgerBackground.addEventListener('click', hamburgerMenuHandler)
 
-hamburgerBackground.addEventListener('click', () => {
-    navigation.classList.remove('navigation--open')
-    navigationBar.classList.remove('navigation__bar--open')
-    hamburgerBackground.classList.remove('hamburger-menu-bg--open')
-    hamburgerButton.classList.remove('closed')
-    body.classList.remove('prevent-scrolling')
-})
-
-
-logo.addEventListener('click', (e) => {
+logo.addEventListener('click', () => {
   if(navigation.classList.contains('navigation--open')) {
-    navigation.classList.remove('navigation--open')
-    navigationBar.classList.remove('navigation__bar--open')
-    hamburgerBackground.classList.remove('hamburger-menu-bg--open')
-    hamburgerButton.classList.remove('closed')
-    body.classList.remove('prevent-scrolling')
+    hamburgerMenuHandler()
   }
 })
-
 
 navigationLinks.forEach((navigationLink) => {
   navigationLink.addEventListener('click', (e) => {
     if(navigation.classList.contains('navigation--open')) {
-      navigation.classList.remove('navigation--open')
-      navigationBar.classList.remove('navigation__bar--open')
-      hamburgerBackground.classList.remove('hamburger-menu-bg--open')
-      hamburgerButton.classList.remove('closed')
-      body.classList.remove('prevent-scrolling')
+      hamburgerMenuHandler()
     }
   })
 })
 
-
-/* HOBBY BUTTONS */
-
-const buttonsHobby = document.querySelectorAll('.btn-hobby--js')
-
-buttonsHobby.forEach((buttonHobby) => {
-  buttonHobby.addEventListener('click', () => {
-    body.classList.add('prevent-scrolling')
-    createPopupWindow()
-  })
-})
-
-//funckja wrzucająca template html, która zależy od tego, który przycisk został kliknięty, jako
-//parametr przyjmuje obiekt
+function hamburgerMenuHandler() {
+  navigation.classList.toggle('navigation--open')
+  navigationBar.classList.toggle('navigation__bar--open')
+  hamburgerBackground.classList.toggle('hamburger-menu-bg--open')
+  hamburgerButton.classList.toggle('closed')
+  body.classList.toggle('prevent-scrolling')
+}
 
 
-const createPopupWindow = () => {
-  const markup = 
-  ` <div class="dupa-1">
-      <div class="modal-window">
-        <button class="modal-window__button btn" href="#">
-            <img class="modal-window__logo-img" src=${svg} alt="Marcelina Hasiak logo, just initials, nothing fency">
-        </button>
-      </div>
-    </div>
-  `
-  body.classList.add('prevent-scrolling')
-  body.insertAdjacentHTML("beforeend", markup)}
+
+
 
 
 
